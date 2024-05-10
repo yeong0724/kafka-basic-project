0124086c10a19e319c58e82d552e0b557036c435
KJY <kid4211@gmail.com>
2024-05-11 01:59:29 +0900

    Kafka - 수동커밋

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # kafka-message-queue
+
+## 1. Docker 관련 주요 커맨드
+docker compose up -d
+
+docker compose down
+
+docker ps
+
+docker logs {컨테이너 id}
+
+docker compose exec {컨테이너 서비스명} {컨테이너 안에서 실행할 명령어}
--- src/main/java/com/fastcampus/kafkahandson/consumer/MyConsumer.java
+++ src/main/java/com/fastcampus/kafkahandson/consumer/MyConsumer.java
@@ -1,8 +1,10 @@
 package com.fastcampus.kafkahandson.consumer;
 
 import com.fastcampus.kafkahandson.model.MyMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 import static com.fastcampus.kafkahandson.model.Topic.MY_JSON_TOPIC;
@@ -10,15 +12,17 @@ import static com.fastcampus.kafkahandson.model.Topic.MY_JSON_TOPIC;
 
 @Component
 public class MyConsumer {
-
     @KafkaListener(
             topics = { MY_JSON_TOPIC },
             groupId = "test-consumer-group"
     )
-    public void accept(ConsumerRecord<String, MyMessage> message) {
+    public void listen(ConsumerRecord<String, MyMessage> message, Acknowledgment acknowledgment) {
         /**
          * Consumer 의 message 는 Key-Value 형태의 쌍으로 이루어져 있다.
          */
         System.out.println("[Main Consumer] Message arrived! - " + message.value());
+
+        // 수동 커밋
+        acknowledgment.acknowledge();
     }
 }
